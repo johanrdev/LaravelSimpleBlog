20f0e6f6a5d589c6ba39a9d8cce52a47cb49155a
johanrdev <johanrothman1988@gmail.com>
2022-09-16 22:09:38 +0200

    Created factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +24,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory(3)->create()->each(function($user) {
+            Post::factory(10)->create(['user_id' => $user->id]);
+
+            Category::factory(3)->create(['user_id' => $user->id]);
+
+            $categories = Category::where('user_id', $user->id)->get();
+
+            Post::where('user_id', $user->id)->each(function($post) use ($categories) {
+                $post->categories()->attach(
+                    $categories->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
+        });
     }
 }
